d8b5651e692c1aa71c71f867ca58da14d6abde39
Bence Somogyi <bence@bencesomogyi.com>
2021-02-28 20:42:08 +0100

    Separate components for readability

---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+import styles from '../styles/Hero.module.css';
+
+export default function Hero() {
+  return <section className={styles.hero}>
+    <div className="container">
+      <div className={styles.hero__logo}>
+        <img className={styles.hero__logo__img} src="/tripelio.svg" alt="Tripelio"/>
+      </div>
+
+      <div className={styles.hero__content}>
+        <div className={styles.hero__content__text}>
+          <div>
+            <h1>Look no further.</h1>
+
+            <p>
+              With a premium recommendation platform we are transforming the recommendation and promotion experience for
+              hosts, service providers and travellers alike.
+            </p>
+
+            <a href="#" className={styles.hero__cta}>
+              All percent inspiration, no percent perspiration.
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.hero__content__preview}>
+          <img src="/preview.png" alt="Product preview"/>
+        </div>
+      </div>
+    </div>
+
+    <div className={styles.hero__map}/>
+  </section>;
+}
