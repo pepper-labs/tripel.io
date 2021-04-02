7eb9e8f16972a8a76ff1399a79313924a563914f
Bence Somogyi <bence@bencesomogyi.com>
2021-04-02 13:50:05 +0200

    Collect basic visitor data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const gtagScript = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', { page_path: window.location.pathname });
+  `;
+  return <>
+    <Head>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}/>
+      <script dangerouslySetInnerHTML={{ __html: gtagScript }}/>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
